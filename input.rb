e3c44e548800521eaddcd8fdd006fbacc93cf727
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2015-12-06 16:47:02 -0500

    refactor to cache input

--- input.rb
+++ input.rb
@@ -4,12 +4,17 @@ require 'net/http'
 module Input
   extend self
   def for_day(day)
-    uri = URI('http://adventofcode.com/day/%s/input'%[day.to_s])
-    req = Net::HTTP::Get.new(uri)
-    req['Cookie'] = File.read('./.session') if File.exist?('./.session')
-    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
-      http.request(req)
-    }
-    res.body
+    cache_file = 'day%d_input.txt'%[day]
+    if File.exist? cache_file
+      File.read(cache_file)
+    else
+      uri = URI('http://adventofcode.com/day/%s/input'%[day.to_s])
+      req = Net::HTTP::Get.new(uri)
+      req['Cookie'] = File.read('./.session') if File.exist?('./.session')
+      res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+        http.request(req)
+      }
+      res.body.tap {|contents| File.write(cache_file, contents) }
+    end
   end
 end
